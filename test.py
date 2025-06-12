6a127d01f0a8da6d393b5fe867cfab27e120ecf4
unisonos <ffgreatdestroyer@gmail.com>
2025-06-12 12:59:25 -0600

    pesto2

--- test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from time import time
-
-from matricula import simulador_matricula
-
-tiempo = time()
-reporte = simulador_matricula()
-tiempo = time() - tiempo
-print(f"simulador_matricula() se completó en {tiempo:.6f}seg")
